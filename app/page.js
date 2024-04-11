b1030c7e2414d59e2e38b9151a0fedb42bc2f01a
sohail9744 <mohammadsohailkhan999@gmail.com>
2024-04-11 12:12:00 +0300

    correction of deployment configuration

--- app/page.js
+++ app/page.js
@@ -2,11 +2,9 @@
 import React, { useState, useEffect } from "react";
 import "./globals.css";
 import Dashboard from "./pages/dashboard";
-import DefaultTheme from "./templates/blueTheme/page";
 export default function Home() {
 
   return (
     <Dashboard />
-    // <DefaultTheme />
   );
 }
