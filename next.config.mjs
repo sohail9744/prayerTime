d8ec758548fbee9e15224bed1b820641663de191
sohail9744 <mohammadsohailkhan999@gmail.com>
2024-05-23 15:07:58 +0300

    cors error add headers in config

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,7 @@ const nextConfig = {
     GOOGLE_CLIENT_ID:
       "608470383876-q5atqm8cd5k02gejn3t4g491fnf3tim6.apps.googleusercontent.com",
     GOOGLE_CLIENT_SECRET: "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",
-    HOST: "https://main.d39t7wzdrffcbl.amplifyapp.com/",
-    NEXTAUTH_URL: "https://main.d39t7wzdrffcbl.amplifyapp.com/",
+    NEXTAUTH_URL: "http://localhost:3000",
     NEXTAUTH_SECRET: "zO5TC/ir3qOHwafVy7Ss2zpohjemaVbGEIdYE3ELMDo=",
     GOOGLE_MAPS_KEY: "AIzaSyD3QQH8b4zZn4CFuhxuZyksdwRRZVbOtWQ",
     HOSTNAME: "http://54.160.190.5:1337", // Backend Hosted on this url
@@ -16,6 +15,27 @@ const nextConfig = {
       "97283c93b3263a7af733c1356b4a3d4d08dd5924d82926502ce67e6f3eb245f211d36bb63270fc1b78902d5199f8c312e8053d7b838e8171b2e9c81ea9a9854ee0cf44823bd3600f30ec670c60edf9aba63a47018a2bfbc4af810e384b9aba3b9ab688ebaf6b0f4c9cb985db8144f1ce98f012b82320d6b24512734354b18b9b", // Local host full access token
   },
   trailingSlash: true,
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
