4496070273ce2524360ff642728fb7c3498fe000
root <root@ip-172-26-9-38.ec2.internal>
2024-05-25 14:15:30 +0000

    updated

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,24 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+ {
+	name: "strapi::security",
+	config: {
+		contentSecurityPolicy: false,	
+	},
+ }, 
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      header: '*',
+      origin: ["https://main.d39t7wzdrffcbl.amplifyapp.com", "http://localhost:3000", "http://54.160.190.5:1337"], // Add your Next.js app domain here
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
