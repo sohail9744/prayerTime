f1bf17f7df2a42360ccddfbbe3e7397620e406ef
root <root@ip-172-31-2-202.eu-west-3.compute.internal>
2024-06-07 07:30:54 +0000

    update middleware.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,7 +5,7 @@ module.exports = [
   {
     name: "strapi::cors",
     config: {
-      origin: ["https://main.d2d0x8dpvmgskk.amplifyapp.com", "http://localhost:3000", "http://13.38.32.2:1337"],
+      origin: ["https://mosqtime.com", "http://localhost:3000"],
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
       headers: ["Content-Type", "Authorization"],
       keepHeadersOnError: true,
