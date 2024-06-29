9148b007d11f629b45bb84be9ce5dd34331e223a
sohail9744 <mohammadsohailkhan999@gmail.com>
2024-06-29 21:54:58 +0300

    middleware issue is fixed

--- middleware.js
+++ middleware.js
@@ -1,6 +1,5 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
-import crypto from "crypto";
 
 export async function middleware(req) {
   // Authentication middleware logic
@@ -11,45 +10,7 @@ export async function middleware(req) {
   if (!token) {
     return NextResponse.redirect(errorPage);
   }
-
-  // CSP middleware logic
-  const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
-  const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-    style-src 'self' 'nonce-${nonce}';
-    img-src 'self' blob: data:;
-    font-src 'self' https://fonts.googleapis.com;
-    object-src 'none';
-    base-uri 'self' https://mosqtime.com;
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-  `;
-
-  const contentSecurityPolicyHeaderValue = cspHeader
-    .replace(/\s{2,}/g, " ")
-    .trim();
-
-  const requestHeaders = new Headers(req.headers);
-  requestHeaders.set("x-nonce", nonce);
-  requestHeaders.set(
-    "Content-Security-Policy",
-    contentSecurityPolicyHeaderValue
-  );
-
-  const response = NextResponse.next({
-    request: {
-      headers: requestHeaders,
-    },
-  });
-
-  response.headers.set(
-    "Content-Security-Policy",
-    contentSecurityPolicyHeaderValue
-  );
-
-  return response;
+  return NextResponse.next();
 }
 
 export const config = {
